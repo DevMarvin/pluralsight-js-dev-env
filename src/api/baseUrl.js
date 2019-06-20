192f8b628419055a1a2fa42687d6c616d6b9fec4
DevMarvin <mmwanzi76@gmail.com>
2019-06-20 20:15:35 +0300

    production deployment

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://enigmatic-oasis-67912.herokuapp.com/';
 }
   
 function getQueryStringParameterByName(name, url) {
