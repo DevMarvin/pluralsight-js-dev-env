5be5d632cc96ae5f7bf3c631d005070ad3a262ad
DevMarvin <mmwanzi76@gmail.com>
2019-06-20 11:43:55 +0300

    hard coding jsno database

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,15 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+    // Hard coding for simplicity. Assume it hits a real Database
+    res.json([
+        {"id": 1, "firstName": "Tim", "lastName": "Ochola", "email": "timochola@gmail.com"},
+        {"id": 2, "firstName": "James", "lastName": "Makanga", "email": "jamesmakanga@gmail.com"},
+        {"id": 3, "firstName": "Edwin", "lastName": "Oluoch", "email": "edwinoluoch@gmail.com"}
+    ]);
+});
+
 app.listen(port, function(err) {
     if (err) {
         console.log(err);
