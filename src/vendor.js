7b97b5b07c4b35a3a5a6c4ab967a82a8467d2e26
DevMarvin <mmwanzi76@gmail.com>
2019-06-20 18:07:38 +0300

    cointains vendor libraries

---
+++ src/vendor.js
@@ -0,0 +1,6 @@
+/* This file contains allthe vendor libraries we are using in this project.
+ Any other files that are not referenced here will be bundled into main.js */
+
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
